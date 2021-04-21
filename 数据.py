4b2303f84846056f571c1c2e0310a1ea113f3498
renmingshuai <57441527+renmingshuai@users.noreply.github.com>
2021-04-21 17:20:25 +0800

    Add files via upload
    提交衣服占比

---
+++ 数据.py
@@ -0,0 +1,266 @@
+date=1
+fuzhuang="羽绒服"
+price=253.6
+number=500
+xslmr=10
+
+date1=2
+fuzhuang1="牛仔裤"
+price1=86.3
+number1=600
+xslmr1=60
+
+date2=3
+fuzhuang2="风衣"
+price2=96.8
+number2=335
+xslmr2=43
+
+date3=4
+fuzhuang3="皮草"
+price3=135.9
+number3=855
+xslmr3=63
+
+date4=5
+fuzhuang4="T血"
+price4=65.8
+number4=632
+xslmr4=63
+
+date5=6
+fuzhuang5="衬衫"
+price5=49.3
+number5=562
+xslmr5=120
+
+date6=7
+fuzhuang6="牛仔裤"
+price6=86.3
+number6=600
+xslmr6=72
+
+date7=8
+fuzhuang7="羽绒服"
+price7=253.6
+number7=500
+xslmr7=69
+
+date8=9
+fuzhuang8="牛仔裤"
+price8=86.3
+number8=600
+xslmr8=35
+
+date9=10
+fuzhuang9="羽绒服"
+price9=235.6
+number9=500
+xslmr9=140
+
+date10=11
+fuzhuang10="牛仔裤"
+price10=86.3
+number10=600
+xslmr10=90
+
+date11=12
+fuzhuang11="皮草"
+price11=135.9
+number11=855
+xslmr11=24
+
+date12=13
+fuzhuang12="T血"
+price12=65.8
+number12=632
+xslmr12=45
+
+date13=14
+fuzhuang13="风衣"
+price13=96.8
+number13=335
+xslmr13=25
+
+date14=15
+fuzhuang14="牛仔裤"
+price14=86.3
+number14=600
+xslmr14=60
+
+date15=16
+fuzhuang15="T血"
+price15=65.8
+number15=632
+xslmr15=129
+
+date16=17
+fuzhuang16="羽绒服"
+price16=253.6
+number16=500
+xslmr16=10
+
+date17=18
+fuzhuang17="风衣"
+price17=96.8
+number17=335
+xslmr17=43
+
+date18=19
+fuzhuang18="T血"
+price18=65.8
+number18=632
+xslmr18=63
+
+date19=20
+fuzhuang19="牛仔裤"
+price19=86.3
+number19=600
+xslmr19=60
+
+date20=21
+fuzhuang20="皮草"
+price20=135.9
+number20=855
+xslmr20=63
+
+date21=22
+fuzhuang21="风衣"
+price21=96.8
+number21=335
+xslmr21=60
+
+date22=23
+fuzhuang22="T血"
+price22=65.8
+number22=632
+xslmr22=58
+
+date23=24
+fuzhuang23="牛仔裤"
+price23=86.3
+number23=600
+xslmr23=140
+
+date24=25
+fuzhuang24="T血"
+price24=65.8
+number24=632
+xslmr24=48
+
+date25=26
+fuzhuang25="风衣"
+price25=96.8
+number25=335
+xslmr25=43
+
+date26=27
+fuzhuang26="皮草"
+price26=135.9
+number26=855
+xslmr26=57
+
+date27=28
+fuzhuang27="羽绒服"
+price27=253.6
+number27=500
+xslmr27=10
+
+date28=29
+fuzhuang28="T血"
+price28=65.8
+number28=632
+xslmr28=63
+
+date29=30
+fuzhuang29="风衣"
+price29=96.8
+number29=335
+xslmr29=78
+
+
+
+
+print("-------------------------------------12月份衣服销售数据-----------------------")
+print("日期","\t","服装名称","\t","价格/件","\t","库存数量","\t","销售量/每日","\t")
+print(date,"\t\t",fuzhuang,"\t\t",price,"\t\t",number,"\t\t",xslmr,"\t\t")
+print(date1,"\t\t",fuzhuang1,"\t\t",price1,"\t\t",number1,"\t\t",xslmr1,"\t\t")
+print(date2,"\t\t",fuzhuang2,"\t\t",price2,"\t\t",number2,"\t\t",xslmr2,"\t\t")
+print(date3,"\t\t",fuzhuang3,"\t\t",price3,"\t\t",number3,"\t\t",xslmr3,"\t\t")
+print(date4,"\t\t",fuzhuang4,"\t\t",price4,"\t\t",number4,"\t\t",xslmr4,"\t\t")
+print(date5,"\t\t",fuzhuang5,"\t\t",price5,"\t\t",number5,"\t\t",xslmr5,"\t\t")
+print(date6,"\t\t",fuzhuang6,"\t\t",price6,"\t\t",number6,"\t\t",xslmr6,"\t\t")
+print(date7,"\t\t",fuzhuang7,"\t\t",price7,"\t\t",number7,"\t\t",xslmr7,"\t\t")
+print(date8,"\t\t",fuzhuang8,"\t\t",price8,"\t\t",number8,"\t\t",xslmr8,"\t\t")
+print(date9,"\t\t",fuzhuang9,"\t\t",price9,"\t\t",number9,"\t\t",xslmr9,"\t\t")
+print(date10,"\t\t",fuzhuang10,"\t\t",price10,"\t\t",number10,"\t\t",xslmr10,"\t\t")
+print(date11,"\t\t",fuzhuang11,"\t\t",price11,"\t\t",number11,"\t\t",xslmr11,"\t\t")
+print(date12,"\t\t",fuzhuang12,"\t\t",price12,"\t\t",number12,"\t\t",xslmr12,"\t\t")
+print(date13,"\t\t",fuzhuang13,"\t\t",price13,"\t\t",number13,"\t\t",xslmr13,"\t\t")
+print(date14,"\t\t",fuzhuang14,"\t\t",price14,"\t\t",number14,"\t\t",xslmr14,"\t\t")
+print(date15,"\t\t",fuzhuang15,"\t\t",price15,"\t\t",number15,"\t\t",xslmr15,"\t\t")
+print(date16,"\t\t",fuzhuang16,"\t\t",price16,"\t\t",number16,"\t\t",xslmr16,"\t\t")
+print(date17,"\t\t",fuzhuang17,"\t\t",price17,"\t\t",number17,"\t\t",xslmr17,"\t\t")
+print(date18,"\t\t",fuzhuang18,"\t\t",price18,"\t\t",number18,"\t\t",xslmr18,"\t\t")
+print(date19,"\t\t",fuzhuang19,"\t\t",price19,"\t\t",number19,"\t\t",xslmr19,"\t\t")
+print(date20,"\t\t",fuzhuang20,"\t\t",price20,"\t\t",number20,"\t\t",xslmr20,"\t\t")
+print(date21,"\t\t",fuzhuang21,"\t\t",price21,"\t\t",number21,"\t\t",xslmr21,"\t\t")
+print(date22,"\t\t",fuzhuang22,"\t\t",price22,"\t\t",number22,"\t\t",xslmr22,"\t\t")
+print(date23,"\t\t",fuzhuang23,"\t\t",price23,"\t\t",number23,"\t\t",xslmr23,"\t\t")
+print(date24,"\t\t",fuzhuang24,"\t\t",price24,"\t\t",number24,"\t\t",xslmr24,"\t\t")
+print(date25,"\t\t",fuzhuang25,"\t\t",price25,"\t\t",number25,"\t\t",xslmr25,"\t\t")
+print(date26,"\t\t",fuzhuang26,"\t\t",price26,"\t\t",number26,"\t\t",xslmr26,"\t\t")
+print(date27,"\t\t",fuzhuang27,"\t\t",price27,"\t\t",number27,"\t\t",xslmr27,"\t\t")
+print(date28,"\t\t",fuzhuang28,"\t\t",price28,"\t\t",number28,"\t\t",xslmr28,"\t\t")
+print(date29,"\t\t",fuzhuang29,"\t\t",price29,"\t\t",number29,"\t\t",xslmr29,"\t\t")
+
+#每件衣服的销售占比是
+
+print("羽绒服的销售占比是",round((xslmr+xslmr7+xslmr9+xslmr16+xslmr27)/(xslmr+xslmr1+xslmr2+xslmr3+xslmr4+xslmr5+xslmr6+xslmr7+xslmr8+xslmr9+xslmr10+xslmr11+xslmr12+xslmr13+xslmr14
+     +xslmr15+xslmr16+xslmr17+xslmr18+xslmr19+xslmr20+xslmr21+xslmr22+xslmr23+xslmr24+xslmr25+xslmr26+xslmr27+xslmr28+xslmr29
+                                )*100,2),"%")
+print("皮草的销售占比是:",round((xslmr3+xslmr11+xslmr20+xslmr26)/(xslmr+xslmr1+xslmr2+xslmr3+xslmr4+xslmr5+xslmr6+xslmr7+xslmr8+xslmr9+xslmr10+xslmr11+xslmr12+xslmr13+xslmr14
+     +xslmr15+xslmr16+xslmr17+xslmr18+xslmr19+xslmr20+xslmr21+xslmr22+xslmr23+xslmr24+xslmr25+xslmr26+xslmr27+xslmr28+xslmr29
+                                )*100,2),"%")
+print("牛仔裤的销售占比是",round((xslmr1+xslmr6+xslmr8+xslmr10+xslmr14+xslmr19+xslmr23)/(xslmr+xslmr1+xslmr2+xslmr3+xslmr4+xslmr5+xslmr6+xslmr7+xslmr8+xslmr9+xslmr10+xslmr11+xslmr12+xslmr13+xslmr14
+     +xslmr15+xslmr16+xslmr17+xslmr18+xslmr19+xslmr20+xslmr21+xslmr22+xslmr23+xslmr24+xslmr25+xslmr26+xslmr27+xslmr28+xslmr29
+                                )*100,2),"%")
+print("风衣的销售占比是",round((xslmr2+xslmr13+xslmr17+xslmr21+xslmr25+xslmr29)/(xslmr+xslmr1+xslmr2+xslmr3+xslmr4+xslmr5+xslmr6+xslmr7+xslmr8+xslmr9+xslmr10+xslmr11+xslmr12+xslmr13+xslmr14
+     +xslmr15+xslmr16+xslmr17+xslmr18+xslmr19+xslmr20+xslmr21+xslmr22+xslmr23+xslmr24+xslmr25+xslmr26+xslmr27+xslmr28+xslmr29
+                                )*100,2),"%")
+print("衬衫的销售占比是",round(xslmr5/(xslmr+xslmr1+xslmr2+xslmr3+xslmr4+xslmr5+xslmr6+xslmr7+xslmr8+xslmr9+xslmr10+xslmr11+xslmr12+xslmr13+xslmr14
+     +xslmr15+xslmr16+xslmr17+xslmr18+xslmr19+xslmr20+xslmr21+xslmr22+xslmr23+xslmr24+xslmr25+xslmr26+xslmr27+xslmr28+xslmr29
+                                )*100,2),"%")
+print("T血的销售占比是",round((xslmr4+xslmr12+xslmr15+xslmr18+xslmr22+xslmr24+xslmr28)/(xslmr+xslmr1+xslmr2+xslmr3+xslmr4+xslmr5+xslmr6+xslmr7+xslmr8+xslmr9+xslmr10+xslmr11+xslmr12+xslmr13+xslmr14
+     +xslmr15+xslmr16+xslmr17+xslmr18+xslmr19+xslmr20+xslmr21+xslmr22+xslmr23+xslmr24+xslmr25+xslmr26+xslmr27+xslmr28+xslmr29
+                                )*100,2),"%")
+
+print("本月总销售额",round(price*xslmr+price1*xslmr1+price2*xslmr2+price3*xslmr3+price4*xslmr4+price5*xslmr5+price6+xslmr6+price7*xslmr7
+      +price8*xslmr8+price9*xslmr9+price10*xslmr10+price11*xslmr11+price12*xslmr12+price13*xslmr13+price14+xslmr14+price15*xslmr15
+      +price16*xslmr16+price17*xslmr17+price18*xslmr18+price19*xslmr19+price20*xslmr20+price21*xslmr21+price22+xslmr22+price23*xslmr23
+      +price24*xslmr24+price25*xslmr25+price26*xslmr26+price27*xslmr27+price28*xslmr28+price29*xslmr29))
+
+print("羽绒服的销售额占比是",round(((xslmr+xslmr7+xslmr9+xslmr16+xslmr27)*price)/(price*xslmr+price1*xslmr1+price2*xslmr2+price3*xslmr3+price4*xslmr4+price5*xslmr5+price6+xslmr6+price7*xslmr7
+      +price8*xslmr8+price9*xslmr9+price10*xslmr10+price11*xslmr11+price12*xslmr12+price13*xslmr13+price14+xslmr14+price15*xslmr15
+      +price16*xslmr16+price17*xslmr17+price18*xslmr18+price19*xslmr19+price20*xslmr20+price21*xslmr21+price22+xslmr22+price23*xslmr23
+      +price24*xslmr24+price25*xslmr25+price26*xslmr26+price27*xslmr27+price28*xslmr28+price29*xslmr29)*100,2),"%")
+print("皮草的销售额占比是",round((price3*(xslmr3+xslmr11+xslmr20+xslmr26))/(price*xslmr+price1*xslmr1+price2*xslmr2+price3*xslmr3+price4*xslmr4+price5*xslmr5+price6+xslmr6+price7*xslmr7
+      +price8*xslmr8+price9*xslmr9+price10*xslmr10+price11*xslmr11+price12*xslmr12+price13*xslmr13+price14+xslmr14+price15*xslmr15
+      +price16*xslmr16+price17*xslmr17+price18*xslmr18+price19*xslmr19+price20*xslmr20+price21*xslmr21+price22+xslmr22+price23*xslmr23
+      +price24*xslmr24+price25*xslmr25+price26*xslmr26+price27*xslmr27+price28*xslmr28+price29*xslmr29)*100,2),"%")
+print("牛仔裤的销售额占比是",round((price1*(xslmr1+xslmr6+xslmr8+xslmr10+xslmr14+xslmr19+xslmr23))/(price*xslmr+price1*xslmr1+price2*xslmr2+price3*xslmr3+price4*xslmr4+price5*xslmr5+price6+xslmr6+price7*xslmr7
+      +price8*xslmr8+price9*xslmr9+price10*xslmr10+price11*xslmr11+price12*xslmr12+price13*xslmr13+price14+xslmr14+price15*xslmr15
+      +price16*xslmr16+price17*xslmr17+price18*xslmr18+price19*xslmr19+price20*xslmr20+price21*xslmr21+price22+xslmr22+price23*xslmr23
+      +price24*xslmr24+price25*xslmr25+price26*xslmr26+price27*xslmr27+price28*xslmr28+price29*xslmr29)*100,2),"%")
+print("风衣的销售额占比",round((price2*(xslmr2+xslmr13+xslmr17+xslmr21+xslmr25+xslmr29))/(price*xslmr+price1*xslmr1+price2*xslmr2+price3*xslmr3+price4*xslmr4+price5*xslmr5+price6+xslmr6+price7*xslmr7
+      +price8*xslmr8+price9*xslmr9+price10*xslmr10+price11*xslmr11+price12*xslmr12+price13*xslmr13+price14+xslmr14+price15*xslmr15
+      +price16*xslmr16+price17*xslmr17+price18*xslmr18+price19*xslmr19+price20*xslmr20+price21*xslmr21+price22+xslmr22+price23*xslmr23
+      +price24*xslmr24+price25*xslmr25+price26*xslmr26+price27*xslmr27+price28*xslmr28+price29*xslmr29)*100,2),"%")
+print("衬衫的销售额占比",round((price5*xslmr5)/(price*xslmr+price1*xslmr1+price2*xslmr2+price3*xslmr3+price4*xslmr4+price5*xslmr5+price6+xslmr6+price7*xslmr7
+      +price8*xslmr8+price9*xslmr9+price10*xslmr10+price11*xslmr11+price12*xslmr12+price13*xslmr13+price14+xslmr14+price15*xslmr15
+      +price16*xslmr16+price17*xslmr17+price18*xslmr18+price19*xslmr19+price20*xslmr20+price21*xslmr21+price22+xslmr22+price23*xslmr23
+      +price24*xslmr24+price25*xslmr25+price26*xslmr26+price27*xslmr27+price28*xslmr28+price29*xslmr29)*100,2),"%")
+print("T血的销售额占比",round((price4*(xslmr4+xslmr12+xslmr15+xslmr18+xslmr22+xslmr24+xslmr28))/(price*xslmr+price1*xslmr1+price2*xslmr2+price3*xslmr3+price4*xslmr4+price5*xslmr5+price6+xslmr6+price7*xslmr7
+      +price8*xslmr8+price9*xslmr9+price10*xslmr10+price11*xslmr11+price12*xslmr12+price13*xslmr13+price14+xslmr14+price15*xslmr15
+      +price16*xslmr16+price17*xslmr17+price18*xslmr18+price19*xslmr19+price20*xslmr20+price21*xslmr21+price22+xslmr22+price23*xslmr23
+      +price24*xslmr24+price25*xslmr25+price26*xslmr26+price27*xslmr27+price28*xslmr28+price29*xslmr29)*100,2),"%")
